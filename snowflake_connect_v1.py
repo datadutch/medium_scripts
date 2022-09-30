84f6161e4ab4ee3ab910b9f0aa8e8c84cc42152c
OokJohan <11424878+bidutch@users.noreply.github.com>
2022-09-30 14:29:16 +0200

    first medium script

---
+++ snowflake_connect_v1.py
@@ -0,0 +1,20 @@
+# https://docs.snowflake.com/en/user-guide/python-connector.html
+
+import snowflake.connector
+import json
+
+with open('/Users/johanvanderkooij/git/medium_scripts/config.json','r') as file:
+    data = json.load(file)
+    user = data['user']
+    password = data['password']
+    account = data['account']
+
+con = snowflake.connector.connect(
+    user = user,
+    password = password,
+    account = account
+)
+
+result = con.cursor().execute("SELECT 1;") 
+result_list = result.fetchall() 
+print(result_list) 
